1c787363611bba2cf3f8b053e3be95f449e12e45
Gustas <gustass1@gmail.com>
2019-11-13 12:21:38 +0100

    feat: configured subscribtions and mutations on the same client

--- pages/cashier.tsx
+++ pages/cashier.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/output.css";
-import { useSubscription } from "@apollo/react-hooks";
+import { useSubscription, useMutation } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 
 const Cashier = () => {
@@ -13,12 +13,33 @@ const Cashier = () => {
     }
   `);
 
+  const [addOrder, data02] = useMutation(MUTATION);
   console.log(data);
   return (
     <div>
-      <button>ASDSAD</button>
+      <button
+        onClick={() => {
+          addOrder({ variables: { isready: false, name: "Pizza" } });
+        }}
+      >
+        ASDSAD
+      </button>
+      <div>
+        {data !== undefined &&
+          data.orders.map((order, index) => <div>{order.name}</div>)}
+      </div>
     </div>
   );
 };
+
+const MUTATION = gql`
+  mutation MyMutation($isready: Boolean, $name: String) {
+    insert_orders(objects: { isready: $isready, name: $name }) {
+      returning {
+        id
+      }
+    }
+  }
+`;
 
 export default Cashier;
