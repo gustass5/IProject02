6cdb99ffe06bdea781dec022b259bf1dc51049d2
Gustas <gustass1@gmail.com>
2019-11-28 08:49:30 +0100

    feat: added sound effect on the lightbridge

--- Arduino/IP/IP.ino
+++ Arduino/IP/IP.ino
@@ -1,20 +1,30 @@
 /*
-* Reference of the light sensor
+  Reference of the light sensor
 */
 const int LDR = 16;
 /*
-* Reference of the LED
+  Reference of the buzzer
+*/
+const int PIN_BUZZER = 3;
+/*
+  Reference of the LED
 */
 const int LED = 4;
+
+const int d4 = 294;
+
 void setup() {
   Serial.begin(9600);
   pinMode(LDR, INPUT);
   pinMode(LED, OUTPUT);
+  pinMode(PIN_BUZZER, OUTPUT);
+  digitalWrite(PIN_BUZZER, LOW);
 }
 void loop() {
   int LDRValue = analogRead(LDR);
-  if(LDRValue <= 200){
+  if (LDRValue <= 200) {
     digitalWrite(LED, HIGH);
+    tone(PIN_BUZZER, d4, 250);
     Serial.println("Move");
     delay(500);
     digitalWrite(LED, LOW);
